f333117e135595c59147d3f560a5d4310e26a0a2
y-soliloquy <engineer.soliloquy@gmail.com>
2025-07-21 23:42:32 +0900

    タイトル修正

--- src/app/piramid/page.tsx
+++ src/app/piramid/page.tsx
@@ -5,7 +5,7 @@ import MenuTable from "../components/menu_table"
 export default async function PiramidPage() {
     return (
         <main className="p-6">
-        <h1 className='text-2xl font-bold p-6'>ピラミッドメニュー</h1>
+        <h1 className='text-2xl font-bold p-6'>ピラミッド法メニュー</h1>
         <div className="p-6">
             <p>筋出力よりも筋肥大向けと言われているメニューです。</p>
         </div>
