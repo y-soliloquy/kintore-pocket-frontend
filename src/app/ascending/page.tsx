ced153665583da99991b4af4e9b7c6916bd6543c
y-soliloquy <engineer.soliloquy@gmail.com>
2025-07-26 14:25:33 +0900

    TOPへ戻るボタンを追加

--- src/app/ascending/page.tsx
+++ src/app/ascending/page.tsx
@@ -1,10 +1,12 @@
 'use server'
 
+import Link from "next/link"
 import MenuTable from "../components/menu_table"
 
 export default async function AscengingPage() {
     return (
         <main className="p-6">
+        <Link className="p-6" href="/">TOPに戻る</Link>
         <h1 className='text-2xl font-bold p-6'>アセンディング法</h1>
         <div className="p-6">
             <p>セットを重ねるたびに扱う重要を増やし、最終的に高重量を扱うセットでトレーニングを締めるメニューです。</p>
--- src/app/piramid/page.tsx
+++ src/app/piramid/page.tsx
@@ -1,10 +1,12 @@
 'use server'
 
+import Link from "next/link"
 import MenuTable from "../components/menu_table"
 
 export default async function PiramidPage() {
     return (
         <main className="p-6">
+        <Link className="p-6" href="/">TOPに戻る</Link>
         <h1 className='text-2xl font-bold p-6'>ピラミッド法メニュー</h1>
         <div className="p-6">
             <p>筋出力よりも筋肥大向けと言われているメニューです。</p>
