a1176aa4852811384b495711c246e2b9bb3e5fcf
y-soliloquy <engineer.soliloquy@gmail.com>
2026-01-07 21:02:29 +0900

    参考動画用の画面を追加

---
+++ src/app/reference/page.tsx
@@ -0,0 +1,8 @@
+export default async function ReferencePage() {
+    return (
+        <main className="p-6">
+            <h1>参考動画画面</h1>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/gIkxdKNnN6o?si=oQ_XfQzZI5JjZblm" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+        </main>
+    )
+}
