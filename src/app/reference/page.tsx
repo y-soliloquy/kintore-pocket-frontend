14da3aaed4e191a2743f8d65018c7dad9fc14584
y-soliloquy <engineer.soliloquy@gmail.com>
2026-01-11 17:24:20 +0900

    TOP画面への遷移追加

--- src/app/reference/page.tsx
+++ src/app/reference/page.tsx
@@ -1,6 +1,12 @@
+'use server'
+
+import Link from "next/link"
+
 export default async function ReferencePage() {
+
     return (
         <main className="p-6">
+            <Link className="p-6" href="/">TOPに戻る</Link>
             <h1>参考動画画面</h1>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/gIkxdKNnN6o?si=oQ_XfQzZI5JjZblm" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
         </main>
